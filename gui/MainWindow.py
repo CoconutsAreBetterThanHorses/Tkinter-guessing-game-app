438da0b72a5dbd319b830c5d5be829c9b2534d7f
Sergey Bakanov <bakanovsergs@gmail.com>
2020-11-30 03:52:15 +0300

    V.1: four options to choose

--- gui/MainWindow.py
+++ gui/MainWindow.py
@@ -1,4 +1,4 @@
-from random import choice
+from random import choice, shuffle
 
 import requests
 from PyQt5.QtCore import QObject, Qt
@@ -7,24 +7,6 @@ from PyQt5.QtWidgets import (
     QLabel, QLineEdit, QPushButton,
     QVBoxLayout
 )
-
-
-def convert(capital):
-    """Help to handle right answer."""
-    braces = ['[', '(']
-    result_capital = []
-
-    # Handle capitals with second name
-    for num, let in enumerate(capital):
-        if let in braces:
-            capital = capital[:num - 1]
-            continue
-
-    # Handle double letter and lowercase input
-    for num, let in enumerate(capital[:-1]):
-        if let != capital[num + 1] and let.isalpha():
-            result_capital.append(let.lower())
-    return result_capital + [capital[-1]]
 
 
 class Countries(QObject):
@@ -46,101 +28,135 @@ class Countries(QObject):
                 self.CACHE.append(data.get('country'))
                 return data
 
+    def get_capital_choices(self, chosen_data):
+        """Get capital names for buttons."""
+        capitals = [chosen_data.get('city')]
+
+        while len(capitals) < 4:
+            data = choice(self.CBC)
+            # Exclude repeats and empty capitals
+            if data.get('city') not in capitals and data.get('city'):
+                capitals.append(data.get('city'))
+
+        # Mix the list
+        shuffle(capitals)
+        return capitals
+
 
 class MainWindow(QMainWindow):
     """ Qt graphic interface."""
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.initData()
         self.initUi()
+        self.initSingltoneData()
+        self.initNewCountryData()
         self.initSignals()
         self.initLayouts()
-        self.initBlock()
-
-    def initData(self):
-        self.countries = Countries()
-        self.country_data = self.countries.get()
-        self.mistakes = 0
 
     def initUi(self):
-        self.resize(300, 250)
+        self.resize(400, 350)
 
-        self.setMinimumSize(300, 200)
+        self.setMinimumSize(300, 250)
         self.setMaximumSize(600, 400)
 
         self.setWindowTitle('Guess the capital')
 
-        self.countryLabel = QLabel(self.country_data.get('country'), )
+        self.countryLabel = QLabel(self)
         self.countryLabel.setStyleSheet("font: 18pt;")
         self.countryLabel.setAlignment(Qt.AlignCenter)
         self.resultLabel = QLabel(self)
         self.resultLabel.setAlignment(Qt.AlignCenter)
 
-        self.capitalEdit = QLineEdit(self)
-
-        self.checkBtn = QPushButton('Check', self)
-        self.newCountryBtn = QPushButton('New Country', self)
+        self.firstCapitalBtn = QPushButton(self)
+        self.secondCapitalBtn = QPushButton(self)
+        self.thirdCapitalBtn = QPushButton(self)
+        self.fourthCapitalBtn = QPushButton(self)
+        self.emptyLabel = QLabel(self)
+        self.newCountryBtn = QPushButton('Set New Country', self)
         self.quitBtn = QPushButton('Quit', self)
 
+    def initSingltoneData(self):
+        self.countries = Countries()
+        self.mistakes = 0
+
+    def initNewCountryData(self):
+        self.country_data = self.countries.get()
+        self.countryLabel.setText(self.country_data.get('country'))
+        self.capital_choices = self.countries.get_capital_choices(
+            self.country_data
+        )
+
+        self.firstCapitalBtn.setText(self.capital_choices[0])
+        self.secondCapitalBtn.setText(self.capital_choices[1])
+        self.thirdCapitalBtn.setText(self.capital_choices[2])
+        self.fourthCapitalBtn.setText(self.capital_choices[3])
+
+        self.newCountryBtn.setEnabled(False)
+
     def initSignals(self):
-        self.checkBtn.clicked.connect(self.onClickCheck)
+        self.firstCapitalBtn.clicked.connect(
+            lambda: self.onClickCheckCountryCapital(0)
+        )
+        self.secondCapitalBtn.clicked.connect(
+            lambda: self.onClickCheckCountryCapital(1)
+        )
+        self.thirdCapitalBtn.clicked.connect(
+            lambda: self.onClickCheckCountryCapital(2)
+        )
+        self.fourthCapitalBtn.clicked.connect(
+            lambda: self.onClickCheckCountryCapital(3)
+        )
         self.newCountryBtn.clicked.connect(self.onClickNewCountry)
         self.quitBtn.clicked.connect(self.onClickQuit)
-
-        self.capitalEdit.textChanged.connect(self.initBlock)
 
     def initLayouts(self):
         self.w = QWidget(self)
 
         self.mainLayout = QVBoxLayout(self.w)
         self.mainLayout.addWidget(self.countryLabel, Qt.AlignVCenter)
-        self.mainLayout.addWidget(self.capitalEdit)
         self.mainLayout.addWidget(self.resultLabel, Qt.AlignVCenter)
-        self.mainLayout.addWidget(self.checkBtn)
+        self.mainLayout.addWidget(self.firstCapitalBtn)
+        self.mainLayout.addWidget(self.secondCapitalBtn)
+        self.mainLayout.addWidget(self.thirdCapitalBtn)
+        self.mainLayout.addWidget(self.fourthCapitalBtn)
+        self.mainLayout.addWidget(self.emptyLabel)
         self.mainLayout.addWidget(self.newCountryBtn)
         self.mainLayout.addWidget(self.quitBtn)
 
         self.setCentralWidget(self.w)
 
-    def initBlock(self):
-        self.checkBtn.setEnabled(True)
-        if not self.getCapitalText():
-            self.checkBtn.setEnabled(False)
-
-    def getCapitalText(self):
-        return self.capitalEdit.text()
-
-    def onClickCheck(self):
-        if self.mistakes == 10:  # attempt amount
-            self.close()
-
-        answer = convert(self.getCapitalText())
-        city = convert(self.country_data.get('city'))
+    def onClickCheckCountryCapital(self, number):
+        answer = self.capital_choices[number]
+        city = self.country_data.get('city')
 
         if answer == city:  # mismatching handler
             self.resultLabel.setText("That's right!")
+            # Unlock the button if answer is right
+            self.newCountryBtn.setEnabled(True)
         else:
             self.mistakes += 1
+            if self.mistakes == 10:  # attempt amount
+                self.close()
+
             self.resultLabel.setText(f"You didn't guess! "
-                                     f"The attempt №{self.mistakes}")
+                                     f"The attempt №{self.mistakes + 1}")
+
 
     def onClickNewCountry(self):
         # Quit from the app if all countries have been before
         if len(self.countries.CACHE) == len(self.countries.CBC):
             self.close()
 
-        self.capitalEdit.setText('')
         self.resultLabel.setText('')
 
-        # Set new country
-        self.country_data = self.countries.get()
-        self.countryLabel.setText(self.country_data.get('country'))
+        # Set new country parameters
+        self.initNewCountryData()
 
     def onClickQuit(self):
         self.close()
 
     def keyPressEvent(self, e):
         if e.key() == Qt.Key_Return:
-            self.onClickCheck()
-        if e.key() == Qt.Key_Space:
             self.onClickNewCountry()
+        if e.key() == Qt.Key_Escape:
+            self.onClickQuit()
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='guess-the-capital',
-    version='0.1.0',
+    version='1.0.0',
     packages=find_packages(),
     py_modules=['guess_the_capital'],
     install_requires=[
